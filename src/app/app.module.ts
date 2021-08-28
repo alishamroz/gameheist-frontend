3033fd5eeefd5ad24c282476f49af09d1680016d
alishamroz <shamroz_ali@outlook.com>
2021-08-28 23:43:25 +0530

    Form Validation & login Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CorouselComponent } from './corousel/corousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CorouselComponent
+    CorouselComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    FlashMessagesModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  name: String = '';
+  email : String = '';
+  password: String = '';
+  mobile: String = '';
+  status: boolean = false;
+  constructor(private flashMessage: FlashMessagesService) {
+    
+  }
 
   ngOnInit(): void {
+  }
+
+  onRegisterSubmit(){
+    // console.log(this.name);
+    const user ={
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      mobile: this.password
+    }
+    if(this.email=='' || this.name=='' || this.mobile=='' || this.password==''){
+      this.flashMessage.show(`Please Fill All details`, {cssClass:'alert-danger', timeout:3000})
+    }
+    console.log(user);
   }
 
 }
