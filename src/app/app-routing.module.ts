4870317d1684d40584e1ca4f74567adb5962ec2f
mshariq90 <mshariq_ms@hotmail.com>
2021-08-28 17:40:30 +0530

    Login Modal Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CorouselComponent } from './corousel/corousel.component';
+
+
 const routes: Routes = [];
 
 @NgModule({
